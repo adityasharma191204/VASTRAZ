7e645b19e91b3a05d72f3a3f20e81e54c92c7ba0
adityasharma191204 <117986637+adityasharma191204@users.noreply.github.com>
2025-01-10 14:48:14 +0530

    Delete vastraz directory

--- vastraz/README.md
+++
@@ -1,3 +0,0 @@
-# Product-Landing-Page
-Ecommerce site 
-
--- vastraz/shop.js
+++
@@ -1,9 +0,0 @@
-let cartBtn=document.getElementById('btnShop');
-for(let i=0;i<cartBtn.length;i++){
-  cartBtn[i].addEventListener('click',addToCart);
-}
-
-function addToCart(event){
-  btn=event.target
-  console.log(btn);
-}
